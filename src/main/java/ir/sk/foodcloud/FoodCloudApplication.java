f4c7c486ee99e139a36049309db463093628c217
Saeed kayvanfar <kayvanfar.sj@gamil.com>
2022-02-08 23:45:01 +0330

    create project base

---
+++ src/main/java/ir/sk/foodcloud/FoodCloudApplication.java
@@ -0,0 +1,13 @@
+package ir.sk.foodcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/sk/foodcloud/FoodCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.sk.foodcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
